d0c8052577156db9af87ce81da37d4f30fc4f41a
Carlos Vesta <vesta.carlos@gmail.com>
2026-07-29 18:53:27 +0200

    day-02

---
+++ day-02/script.js
@@ -0,0 +1,34 @@
+let name = "Carlos";
+let age = 37;
+const isStudent = true;
+let favoritProgrammingLanguage = "Javascript";
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favoritProgrammingLanguage);
+
+name = "Marta";
+age = 25;
+// isStudent = false; // Will return an error
+favoritProgrammingLanguage = "PHP";
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favoritProgrammingLanguage);
+
+
+let person = {
+    name: "Melvin",
+    age: 14,
+    isStudent: true
+}
+
+let cars = ["Toyota", "Mazda", "Chevorlet"];
+
+console.log(person);
+console.log(cars);
+
+cars = ["Ford", "Ferrari"];
+console.log(cars);
