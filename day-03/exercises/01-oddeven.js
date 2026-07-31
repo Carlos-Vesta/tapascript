5d70384e8ffc6f794244ee1eae50eddec4355e9c
Carlos Vesta <vesta.carlos@gmail.com>
2026-07-31 20:40:17 +0200

    01-oddeven.js

---
+++ day-03/exercises/01-oddeven.js
@@ -0,0 +1,12 @@
+
+/*
+    # 1. Odd or Even?
+
+    Take a number and find if the number is an odd or even number.
+    Print the number and result in the console.
+*/
+
+let oddEven = 23;
+
+let answer = oddEven % 2 === 0 ? "Even Number" : "Odd Number";
+console.log(`${oddEven} it's a ${answer}.`);
