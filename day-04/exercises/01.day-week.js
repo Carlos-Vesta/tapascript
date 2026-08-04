a065b9dabcbeccb049be8b4e34517e023c916cff
Carlos Vesta <vesta.carlos@gmail.com>
2026-08-04 12:04:00 +0200

    01.day-week.js

---
+++ day-04/exercises/01.day-week.js
@@ -0,0 +1,12 @@
+
+let day = "Monday";
+
+switch (day) {
+    case "monday":
+        console.log("It's the start of the week.");
+        break;
+
+    default:
+        console.log("It's a normal day.");
+
+}
